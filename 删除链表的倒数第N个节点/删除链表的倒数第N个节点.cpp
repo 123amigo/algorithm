8e0ae8e9687e842bbf4c00dddd95a9e153789925
123amigo <1779312518@qq.com>
2022-10-17 19:38:09 +0800

    update

---
+++ 删除链表的倒数第N个节点/删除链表的倒数第N个节点.cpp
@@ -0,0 +1,68 @@
+﻿/*
+给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+
+示例 1：
+输入：head = [1,2,3,4,5], n = 2
+输出：[1,2,3,5]
+
+示例 2：
+输入：head = [1], n = 1
+输出：[]
+
+示例 3：
+输入：head = [1,2], n = 1
+输出：[1]
+
+提示：
+
+链表中结点的数目为 sz
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <iostream>
+#include <vector>
+#include <assert.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+/*
+1.遍历得到链表长度
+2.再遍历找到需删除节点的上一个节点
+3.最后将上一节点连接到需删除节点的下一节点
+*/
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int length = 0;
+        ListNode* p = head;
+        while (p != nullptr) {
+            length++;
+            p = p->next;
+        }
+        if (length == n) {
+            return head->next;
+        }
+        p = head;
+        for (int i = 0; i < length - n - 1; i++) {
+            p = p->next;
+        }
+        p->next = p->next->next;
+        return head;
+    }
+};
---
+++ 反转链表/反转链表.cpp
@@ -0,0 +1,70 @@
+﻿/*
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+示例 1：
+输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+
+示例 2：
+输入：head = [1,2]
+输出：[2,1]
+
+示例 3：
+输入：head = []
+输出：[]
+ 
+提示：
+链表中节点的数目范围是 [0, 5000]
+-5000 <= Node.val <= 5000
+
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/*
+使用栈来进行反转
+*/
+#include <iostream>
+#include <vector>
+#include <assert.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+#include <stack>
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        stack <ListNode*> stackLN;
+        ListNode* p = head;
+        while (p != nullptr) {
+            stackLN.push(p);
+            p = p->next;
+        }
+        if (stackLN.empty()) {
+            return NULL;
+        }
+        ListNode* temp = stackLN.top();
+        head = temp;
+        stackLN.pop();
+        while (!stackLN.empty()) {
+            temp->next = stackLN.top();
+            temp = temp->next;
+            stackLN.pop();
+        }
+        temp->next = NULL;
+        return head;
+
+    }
+};
