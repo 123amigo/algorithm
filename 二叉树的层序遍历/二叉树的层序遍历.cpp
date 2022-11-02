42f89bf38411d8fa83f85092f8cbf85c401644bb
123amigo <1779312518@qq.com>
2022-11-02 23:18:23 +0800

    update

---
+++ 二叉树的层序遍历/二叉树的层序遍历.cpp
@@ -0,0 +1,56 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+
+示例 1：
+输入：root = [3,9,20,null,null,15,7]
+输出：[[3],[9,20],[15,7]]
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> target;
+        if (!root)
+            return target;
+
+        queue<TreeNode*> que;
+        que.push(root);
+        while (!que.empty()) {
+            // 当前队列中的元素都是同一层的，n为该层元素数目
+            int n = que.size();
+            // 定义一个容器存储该层的节点的val值
+            vector<int> tv;
+            while (n--) {
+                TreeNode* tn = que.front(); que.pop();
+                tv.push_back(tn->val);
+                if (tn->left)
+                    que.push(tn->left);
+                if (tn->right)
+                    que.push(tn->right);
+            }
+            target.push_back(tv);
+        }
+        return target;
+    }
+};
+
---
+++ 对称二叉树/对称二叉树.cpp
@@ -0,0 +1,44 @@
+﻿/*
+给你一个二叉树的根节点 root ， 检查它是否轴对称。
+示例 1：
+输入：root = [1,2,2,3,4,4,3]
+输出：true
+*/
+#include <iostream>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetricCom(TreeNode* left, TreeNode* right) {
+        if (left == nullptr && right == nullptr)
+            return true;
+        if (left == nullptr || right == nullptr || left->val != right->val)
+            return false;
+        return isSymmetricCom(left->left, right->right) && isSymmetricCom(left->right, right->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr)
+            return true;
+        return isSymmetricCom(root->left, root->right);
+    }
+
+};
