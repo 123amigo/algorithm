345e3dd62cd5d9330b1f0b4d9325786b8ea9c67f
123amigo <1779312518@qq.com>
2022-11-19 22:53:28 +0800

    update

---
+++ 将有序数组转换为二叉搜索树/将有序数组转换为二叉搜索树.cpp
@@ -0,0 +1,31 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+#include <stdio.h>
+#include <stdlib.h>
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+
+};
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
+    if (0 == numsSize)
+        return NULL;
+
+    struct TreeNode* root = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    root->val = nums[numsSize / 2];
+
+    root->left = sortedArrayToBST(nums, numsSize / 2);
+    root->right = sortedArrayToBST(nums + numsSize / 2 + 1, numsSize / 2 - !(numsSize % 2));
+    return root;
+}
+
